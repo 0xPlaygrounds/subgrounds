0f3af47db9c95ff0e66ad0ba591cdaba3f95425b
0xMochan <mochanbot@gmail.com>
2023-03-03 11:29:13 -0500

    chore: reorder version to trigger a release

--- subgrounds/__init__.py
+++ subgrounds/__init__.py
@@ -1,11 +1,11 @@
 from subgrounds.subgraph import FieldPath, Subgraph, SyntheticField
 from subgrounds.subgrounds import Subgrounds
 
+__version__ = "1.0.3"
+
 __all__ = [
     "FieldPath",
     "Subgraph",
     "Subgrounds",
     "SyntheticField",
 ]
-
-__version__ = "1.0.3"

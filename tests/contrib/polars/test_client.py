8f69ceba5faf6d903b04f84d33e13d88d5bbee19
Evan-Kim2028 <ekcopersonal@gmail.com>
2023-09-24 17:09:07 -0400

    feat: test `query_df()`

--- tests/contrib/polars/test_client.py
+++ tests/contrib/polars/test_client.py
@@ -1,11 +1,6 @@
-from polars_client import SubgroundsPolars
-from subgrounds.subgraph import FieldPath, Subgraph
+from subgrounds.contrib.polars.client import PolarsSubgrounds
 
-from polars_utils import *
-
-import polars as pl
-
-sg = SubgroundsPolars()
+sg = PolarsSubgrounds()
 
 snx_endpoint = "https://api.thegraph.com/subgraphs/name/synthetix-perps/perps"
 
@@ -13,41 +8,19 @@ snx = sg.load_subgraph(
     url=snx_endpoint,
 )
 
-trades_json = sg.query_json(
+
+trades_df = sg.query_df(
     [
         # set the first parameter to a larger size to query more rows.
         snx.Query.futuresTrades(
-            first=2500,
+            first=5000,
             orderBy="timestamp",
             orderDirection="desc",
-            # where=[{"timestamp_lte": "1694131200"}],  # 1694131200 = 9/8/23
         )
     ]
 )
 
-json_trades_key = list(trades_json[0].keys())[0]
-trades_df = pl.from_dicts(
-    trades_json[0][list(trades_json[0].keys())[0]], infer_schema_length=None
-)
+print(trades_df.head(10))
 
-fmted_df = fmt_dict_cols(trades_df)
-
-# # polars calculations to convert big ints. All synthetix big ints are represented with 10**18 decimals.
-fmted_df = fmted_df.with_columns(
-    [
-        (pl.col("margin") / 10**18),
-        (pl.col("size") / 10**18),
-        (pl.col("price") / 10**18),
-        (pl.col("positionSize") / 10**18),
-        (pl.col("realizedPnl") / 10**18),
-        (pl.col("netFunding") / 10**18),
-        (pl.col("feesPaidToSynthetix") / 10**18),
-        # convert timestamp to datetime
-        pl.from_epoch("timestamp").alias("datetime"),
-    ]
-)
-
-print(fmted_df.shape)
-print(fmted_df.head(5))
 
 print("done")
